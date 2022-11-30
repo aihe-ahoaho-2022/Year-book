d3890f1984b49695b3be2664310bdba9b81dcc2a
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2022-12-01 10:40:58 +1300

    add

--- client/components/ProfileEdit.jsx
+++ client/components/ProfileEdit.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function ProfileEdit() {
+export default function FruitEditor(props) {
+  // Ready up React state
+  const [quote, setQuote] = useState({ name: "", description: "" })
+  const [name, setName ] = useState({ name: "", description: "" })
+
+  
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(quote, name);
+  };
+
   return (
     <>
-      <h1>Final PAGE :D</h1>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <p>Create Editor:</p>
+          <ul>
+            <li>
+              Name:
+              <input
+                value={quote.name}
+                onChange={e => setQuote({
+                  ...quote,
+                  name: e.target.value
+                })}
+              ></input>
+            </li>
+            <li>
+              Description:
+              <input
+                value={  const [quote, setQuote] = useState({ name: "", description: "" })
+                .description}
+                onChange={e => setQuote({
+                  ...quote,
+                  description: e.target.value
+                })}
+              ></input>
+            </li>
+          </ul>
+          <button type="submit" >Save</button>
+        </form>
+      </div>
     </>
   )
+
 }
