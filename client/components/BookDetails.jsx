a30e69a5d11509691cbca7ff8177c4288dab5086
anna-li-22 <111815022+anna-li-22@users.noreply.github.com>
2022-12-01 14:49:46 +1300

    bookDetail frame

--- client/components/BookDetails.jsx
+++ client/components/BookDetails.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { useNavigate, useParams } from 'react-router-dom'
+import Profile from './Profile'
+import { getProfilesByBookId } from '../apis/home'
+import { setProfiles } from '../actions'
 
 export default function BookDetails() {
+  const params = useParams()
+  const bookId = Number(params.bookId)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  function displayProfiles() {
+    getProfilesByBookId()
+      .then((profiles) => {
+        dispatch(setProfiles(profiles))
+      })
+      .catch((err) => {
+        console.error(err.message)
+      })
+  }
+  ge
+
   return (
     <>
-      <h1>Final PAGE :D</h1>
+      <h1>Yearbook Title</h1>
+      <Profile profile={profile} />
     </>
   )
 }
