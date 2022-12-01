2a479934e030339d98f1a1856d4ba54cd57d9e3c
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2022-12-01 21:28:59 +1300

    add

--- client/components/BookEdit.jsx
+++ client/components/BookEdit.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
+import ImageUpload from './ImageUpload'
+
+
 export default function BookEdit() {
   return (
     <>
+      <ImageUpload />
       <h1>Final PAGE :D</h1>
     </>
   )
