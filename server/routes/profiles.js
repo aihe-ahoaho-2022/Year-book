01834626d6877972cb07439b7437a7cd1e561831
James Koay <88022942+james-koay@users.noreply.github.com>
2022-12-01 21:56:23 +1300

    updated routes

--- server/routes/profiles.js
+++ server/routes/profiles.js
@@ -1,8 +1,42 @@
 const express = require('express')
+const path = require('path')
 
 const { getProfilesByBookId, getProfileById } = require('../db/db')
+const { imageUpload } = require('../db/db')
 
 const router = express.Router()
+
+const multer = require('multer')
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, '../public/images'))
+  },
+  filename: (req, file, cb) => {
+    console.log(file)
+    cb(null, Date.now() + path.extname(file.originalname))
+  },
+})
+
+const upload = multer({ storage: storage })
+
+// GET /api/v1/profiles/:profileId/edit
+router.get('/:profileId/edit', (req, res) => {
+  res.render('upload')
+})
+
+// POST /api/v1/profiles/:profileId/edit
+router.post('/:profileId/edit', upload.single('image'), (req, res) => {
+  imageUpload(req.body.image)
+    .then(() => {
+      res.send('image uploaded')
+    })
+    .catch((err) => {
+      console.error(err.message)
+      res.status(500).json({
+        message: 'Something went wrong',
+      })
+    })
+})
 
 // GET /api/v1/profiles/book/:bookId
 router.get('/book/:bookId', (req, res) => {
