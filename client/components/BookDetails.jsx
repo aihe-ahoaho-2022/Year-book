7fc10708880ee3369c26427c6f3120554cdecb3c
anna-li-22 <111815022+anna-li-22@users.noreply.github.com>
2022-12-02 15:58:40 +1300

    bookdetails component listing things without css

--- client/components/BookDetails.jsx
+++ client/components/BookDetails.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useNavigate, useParams, Link } from 'react-router-dom'
 // import Profile from './Profile'
 import styles from './BookDetails.module.scss'
 import { fetchProfiles, setProfiles } from '../actions/profile'
 
 export default function BookDetails(data) {
   const params = useParams()
+  const navigate = useNavigate()
   const bookId = Number(params.bookid)
   const dispatch = useDispatch()
   // const [isLoading, setIsLoading] = useState(true)
@@ -20,16 +21,28 @@ export default function BookDetails(data) {
     dispatch(fetchProfiles(bookId))
   }, [])
 
-  const displayProfiles = profiles?.map((profile, index) => (
-    <div key={profile.id + index}>
-      <p>profiles here</p>
-      <img
-        className={styles.image}
-        src={profile.image}
-        alt={`${profile.name}`}
-      ></img>
-      <h1>Name:{profile.name}</h1>
-    </div>
+  // function addAnimalToRedux(animal) {
+  //   dispatch(updateAnimals(animal))
+  //   navigate(`/final/${profile.id}`)
+  // }
+  // function handleClick (event){
+
+  // }
+
+  const displayProfiles = profiles?.map((profile) => (
+    <ul key={profile.id}>
+      <Link to={`/profiles/${profile.id}`}>
+        <div>
+          <p>profiles here</p>
+          <img
+            className={styles.image}
+            src={profile.image}
+            alt={`${profile.name}`}
+          ></img>
+          <h1>Name:{profile.name}</h1>
+        </div>
+      </Link>
+    </ul>
   ))
   console.log(displayProfiles)
 
@@ -38,10 +51,12 @@ export default function BookDetails(data) {
       <div className='Title'>
         <h1 className={styles.heading}>Yearbook Title</h1>
       </div>
-      <div className='profiles_containers'>
-        {displayProfiles}
-        {/* <Profile profile={profiles} /> */}
-      </div>
+
+      <div className={styles.container_profiles}>{displayProfiles}</div>
+      <Link to={`/profiles/1/edit`}>
+        <div className={styles.container_profiles}>AddNew</div>
+      </Link>
+
       <div className='comments_containers'>{/* <input>posts</input> */}</div>
     </>
   )
