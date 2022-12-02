761b4ef23c4fdd529282801c3f1bbb1f4cc8c049
Ben Williams <111544245+ben-bwilly-williams@users.noreply.github.com>
2022-12-02 15:51:52 +1300

    Hide edit button with auth

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -46,6 +46,7 @@ export default function App() {
         <Route path='/*' element={<NotFound />} />
       </Routes>
       {/* </Container> */}
+      {/* </AppShell> */}
     </>
   )
 }
--- client/components/ProfileDetails.jsx
+++ client/components/ProfileDetails.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useParams, Link } from 'react-router-dom'
+// import { IfAuthenticated, IfNotAuthenticated } from './Authenticated'
+
 import { getProfileById } from '../apis/profile'
 import styles from './ProfileDetails.module.scss'
 
@@ -21,41 +23,48 @@ export default function ProfileDetails() {
 
   return (
     <>
-      <Link to='/:bookid'>
-        <div className={styles.fullscreen}>
-          <div className={styles.container}>
-            <img
-              className={styles.image}
-              src={profile.image}
-              // src={`/images/${profile.image}`}
-              alt='User profile'
-            />
-            <section className={styles.textbox}>
-              <h1 className={styles.heading}>{profile.name}</h1>
-              <h2 className={styles.quote}>{`"${profile.quote}"`}</h2>
-              <hr className={styles.hr} />
-              <h2 className={styles.blurb}>{profile.blurb}</h2>
-              <ul className={styles.list}>
-                <a href={profile.linkedin_url}>
-                  <li className={styles.li}>LinkedIn</li>
-                </a>
-                <a href={profile.twitter_url}>
-                  <li className={styles.li}>Twitter</li>
-                </a>
-                <a href={profile.instagram_url}>
-                  <li className={styles.li}>Instagram</li>
-                </a>
-                <a href={profile.facebook_url}>
-                  <li className={styles.li}>Facebook</li>
-                </a>
-                <a href={profile.github_url}>
-                  <li className={styles.li}>GitHub</li>
-                </a>
-              </ul>
-            </section>
-          </div>
+      {/* <Link to='/:bookid'>
+        <div className={styles.fullscreen}> */}
+      <div className={styles.container}>
+        <div>
+          <img
+            className={styles.image}
+            src={profile.image}
+            // src={`/images/${profile.image}`}
+            alt='User profile'
+          />
+          {/* <IfAuthenticated> */}
+          <Link to='/:bookid/edit'>
+            <button className={styles.button}>Edit profile</button>
+          </Link>
+          {/* </IfAuthenticated> */}
         </div>
-      </Link>
+        <section className={styles.textbox}>
+          <h1 className={styles.heading}>{profile.name}</h1>
+          <h2 className={styles.quote}>{`"${profile.quote}"`}</h2>
+          <hr className={styles.hr} />
+          <h2 className={styles.blurb}>{profile.blurb}</h2>
+          <ul className={styles.list}>
+            <a href={profile.linkedin_url}>
+              <li className={styles.li}>LinkedIn</li>
+            </a>
+            <a href={profile.twitter_url}>
+              <li className={styles.li}>Twitter</li>
+            </a>
+            <a href={profile.instagram_url}>
+              <li className={styles.li}>Instagram</li>
+            </a>
+            <a href={profile.facebook_url}>
+              <li className={styles.li}>Facebook</li>
+            </a>
+            <a href={profile.github_url}>
+              <li className={styles.li}>GitHub</li>
+            </a>
+          </ul>
+        </section>
+      </div>
+      {/* </div>
+      </Link> */}
     </>
   )
 }
