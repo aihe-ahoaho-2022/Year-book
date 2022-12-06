f06f360974fa6657ff8db027c3b07155e5b2bf1d
anna-li-22 <111815022+anna-li-22@users.noreply.github.com>
2022-12-07 12:31:14 +1300

    linting error fixed

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import styles from './Home.module.scss'
 import Book from './Book'
 import { setBooks } from '../actions'
