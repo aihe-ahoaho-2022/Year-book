2a32db65210af454c2e23e3c7fbd4e7a1a2e362f
anna-li-22 <111815022+anna-li-22@users.noreply.github.com>
2022-12-07 22:10:02 +1300

    db testing turned green

--- server/db/__tests__/db.test.js
+++ server/db/__tests__/db.test.js
@@ -3,10 +3,13 @@ const {
   getBooks,
   getBookById,
   deleteBook,
+  addBook,
+  putBookById,
   getCommentsByBookId,
   deleteProfile,
   getProfileById,
   addProfile,
+  putProfileById,
   postComment,
 } = require('../db')
 
@@ -68,7 +71,6 @@ describe('deleteBook', () => {
         return getBooks(testDb)
       })
       .then((books) => {
-        console.log(books)
         expect(books).toHaveLength(3)
       })
   })
@@ -129,6 +131,66 @@ describe('add comments to the database', () => {
     }
     return postComment(mockComments, testDb).then((id) => {
       expect(id[0]).toBe(9)
+    })
+  })
+})
+
+describe('add book to the database', () => {
+  it('add book back to database', () => {
+    const mockBook = {
+      name: 'EDA Aihe Ahoaho 2022',
+      image: '/images/ahoaho-book.png',
+      theme: 'red',
+      auth0_id: 'authtestid1',
+    }
+
+    return addBook(mockBook, testDb).then((id) => {
+      expect(id[0]).toBe(5)
+    })
+  })
+})
+
+describe('update profiles by id to the database', () => {
+  it('update profiles by id database', () => {
+    const mockProfile = {
+      id: 5,
+      bookId: 1,
+      name: 'Banana',
+      image: '',
+      ownerId: '',
+      quote: ``,
+      blurb: '',
+      linkedinUrl: '',
+      twitterUrl: '',
+      instagramUrl: '',
+      facebookUrl: '',
+      githubUrl: 'https://github.com/jatin-puri-coder',
+    }
+
+    return putProfileById(mockProfile.id, mockProfile, testDb).then(() => {
+      return getProfileById(mockProfile.id, testDb).then((profile) => {
+        expect(profile).toHaveLength(1)
+        expect(profile[0].name).toContain('Banana')
+      })
+    })
+  })
+})
+
+describe('update book by id to the database', () => {
+  it('update book by id database', () => {
+    const mockBook = {
+      id: 4,
+      name: 'updated value',
+      image: '/images/dog-warts-book.png',
+      theme: 'purple',
+      auth0_id: 'authtestid4',
+    }
+
+    return putBookById(mockBook.id, mockBook, testDb).then(() => {
+      return getBookById(mockBook.id, testDb).then((book) => {
+        console.log(book)
+        expect(book.name).toContain('updated value')
+      })
     })
   })
 })
