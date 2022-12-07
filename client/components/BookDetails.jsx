0e4590f747b0961ecd90f3864a0423537d5527ce
anna-li-22 <111815022+anna-li-22@users.noreply.github.com>
2022-12-07 13:22:09 +1300

    quote fixed

--- client/components/BookDetails.jsx
+++ client/components/BookDetails.jsx
@@ -42,7 +42,6 @@ export default function BookDetails() {
               Quote:
               <br />
               {profile.quote}
-              <hr />
               Click to edit!
             </p>
           </div>
