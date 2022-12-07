d0dd162e553d27ff718ea54e88ed4e27884166ae
anna-li-22 <111815022+anna-li-22@users.noreply.github.com>
2022-12-07 23:40:38 +1300

    css tidy up

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -8,6 +8,11 @@ export default function Footer() {
       <div>
         <footer className={styles.footer}>
           <div className={styles.title}>
+            <img
+              className='footer_pattern'
+              alt='pattern'
+              src='../../images/pattern.svg'
+            ></img>
             <h3>Designed by [ Anna, Ben, James, Razel, Tom, Yumi ] </h3>
           </div>
         </footer>
