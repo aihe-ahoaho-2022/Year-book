64c8e01282c3d7bcd4179665463311a5613d4105
James Koay <88022942+james-koay@users.noreply.github.com>
2022-12-08 09:00:58 +1300

    updated multer directory

--- server/routes/multer.js
+++ server/routes/multer.js
@@ -1,11 +1,9 @@
 const path = require('path')
 
-
-
 const multer = require('multer')
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, '../public/images'))
+    cb(null, path.join(__dirname, '../../storage/images'))
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + '-' + file.originalname)
